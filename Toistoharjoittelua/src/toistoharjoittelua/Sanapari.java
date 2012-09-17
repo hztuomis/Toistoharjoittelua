2708abb9d08328c31e88e4aad11516a148ac98b7
Heikki Tuomisto <heikki.tuomisto@gmail.com>
2012-09-17 11:42:36 +0300

    yritetään valmistella Kayttoliityman siirtoa

--- Toistoharjoittelua/src/toistoharjoittelua/Sanapari.java
+++ Toistoharjoittelua/src/toistoharjoittelua/Sanapari.java
@@ -44,51 +44,12 @@ public class Sanapari {
         return vastaus.equals("");
     }
 
-    public void annaSanapari () {    
-        
-        Scanner lukija = new Scanner(System.in);
-        
-        System.out.print("Anna kysyttävä sana: ");
-        String sana = lukija.nextLine();
-        System.out.println("Sana oli: " + sana);
-        /* tähän mahdollisia tarkastuksia */
-        this.kysymys = sana;
-
-        System.out.print("Anna vastine: ");
-        String vastine = lukija.nextLine();
-        System.out.println("Vastine oli: " + vastine);
-        /* tähän mahdollisia tarkastuksia */
-        this.vastaus = vastine;
-        /* hyväksytkö vai hylkäätkö sanaparin?*/
-    }
-    
     public boolean vastausOikein (String kysymys, 
             String ehdotettuVastaus) {
          return this.kysymys.equals(kysymys) && 
                  this.vastaus.equals(ehdotettuVastaus);
     }
-    
-    public void kysy() {
-        System.out.print(this.kysymys + "?: ");
-    }
-    
-    public String ehdotettuVastaus() {
-        kysy();
-        Scanner lukija = new Scanner(System.in);
-        return lukija.nextLine();
-    }
-    
-    public void kyseleJaTarkastaSanapari() {
-        String ehdotus = ehdotettuVastaus();
-        if (vastausOikein(getKysymys(), ehdotus )) {
-        System.out.println("Oikein");
-        } else {
-           System.out.println("Väärin");
-           System.out.println("Oikea vastaus on: " + getVastaus());
-        }
-    }
-
-    
+        
     public String toString() {
         return kysymys + "  ->  " + vastaus;
     }
--- Toistoharjoittelua/src/toistoharjoittelua/Sanaparilista.java
+++ Toistoharjoittelua/src/toistoharjoittelua/Sanaparilista.java
@@ -5,6 +5,7 @@
 package toistoharjoittelua;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
@@ -13,25 +14,50 @@ import java.util.ArrayList;
 public class Sanaparilista {
    
     private ArrayList<Sanapari> sanaParit = new ArrayList<Sanapari>();
-   
-    public void lueSanaparitListaan() {
-        while (true) {
-            Sanapari syoteRivi = new Sanapari("","");
-            syoteRivi.annaSanapari();
-            if ( (syoteRivi.kysymysTyhja()) || syoteRivi.vastausTyhja() ) {
-                System.out.println("Lopetetaan!");
-                break; // <<<<<<<<<<<<< POISTUTAAN
-            }
-            /* samaa paria ei saa lisätä toiseen kertaan!*/
-            if ( ! onJoListassa ( syoteRivi) ) { 
-                sanaParit.add(syoteRivi);
-                System.out.println(syoteRivi);
-            } else {
-                System.out.println("Rivi on jo listassa, " + 
-                        "sitä ei lisätä uudestaan");
-            }    
-        }
+    private ArrayList<String> sananKoiraParit = new ArrayList<String>();
+    private HashMap<String,ArrayList<String>> sanat = new HashMap();
+    private HashMap<String,Integer> kuinkaMontaKertaaVastattuVaarin = 
+            new HashMap();
+    
+    public void LisaaPariListaan (String kysymys, 
+            String vastaus) {
+        Sanapari sp = new Sanapari( kysymys, vastaus);
+        sanaParit.add(sp);     
     }
+
+    public int SanaPariListanKoko (){
+        return sanaParit.size();
+    }
+    
+//    public void testaillaanHashMappia() {
+//        sananKissaParit.add("cat");
+//        sananKissaParit.add("a cat");
+//        sananKoiraParit.add("dog");
+//        sananKissaParit.add("katten");
+//        sanat.put("kissa", sananKissaParit);
+//        sanat.put("koira", sananKoiraParit);
+//        kuinkaMontaKertaaVastattuVaarin.put("kissa", 5);
+//        int vanhaLkm = kuinkaMontaKertaaVastattuVaarin.get("kissa");
+//        kuinkaMontaKertaaVastattuVaarin.put("kissa", (vanhaLkm + 1));
+//        
+//        System.out.println("Sanaa kissa arvattu väärin: " +
+//             kuinkaMontaKertaaVastattuVaarin.get("kissa"));
+//        System.out.println("Sanaa koira arvattu väärin: " +
+//             kuinkaMontaKertaaVastattuVaarin.get("koira"));
+//        
+//        for (int i = 0; i < 1000; i++) {
+//            sananKissaParit = new ArrayList();
+//            
+//        }
+//        
+//        
+//        
+//        for (String avain : sanat.keySet()) {
+//            System.out.println(sanat.get(avain));
+//        }
+//        
+//    }}
+   
     
     public void tulostaSanapariLista() {
         for (int i = 0; i < sanaParit.size(); ++i) {
@@ -39,12 +65,10 @@ public class Sanaparilista {
         }
     }    
 
-    public void kyseleJaTarkastaSanapariLista() {
-        for (int i = 0; i < sanaParit.size(); ++i) {
-            sanaParit.get(i).kyseleJaTarkastaSanapari();
-        }
+    public Sanapari AnnaSanapariListasta (int i) {
+        return sanaParit.get(i);
     }
-
+    
     public boolean onJoListassa(Sanapari syoteRivi) {
         for (int i = 0; i < sanaParit.size(); ++i) {
            if (sanaParit.get(i).vastausOikein(syoteRivi.getKysymys(), 
